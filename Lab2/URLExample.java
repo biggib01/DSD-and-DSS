8958a8427f53bdfdcd7012949fccb08d313d5335
biggib01 <77378396+biggib01@users.noreply.github.com>
2021-09-14 14:12:05 +0700

    Update URLExample.java

--- Lab2/URLExample.java
+++ Lab2/URLExample.java
@@ -42,7 +42,7 @@ public class URLExample
   public static void insertsql(Vector<Iris> Input) throws SQLException {
     String url = "jdbc:mysql://localhost:3306/?user=root";
     String user = "root";
-    String password = "Nope0970948728**";
+    String password = "UrPassWord";
     Connection myConn = null;
     Statement myStmt = null;
 
